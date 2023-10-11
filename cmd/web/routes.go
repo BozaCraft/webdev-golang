db727433df559dacd208331cb681a0c9c251fef7
Bozidar <dzulijan111@gmail.com>
2023-10-11 15:13:58 -0700

    zavrsio 4.2

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,19 +3,21 @@ package main
 import (
 	"net/http"
 	"github.com/BozaCraft/go-course/pkg/config"
-	"github.com/bmizerany/pat"
 	"github.com/BozaCraft/go-course/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	mux.Use(middleware.Recoverer)
 
 
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
